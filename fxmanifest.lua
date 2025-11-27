0cf229e96ed1b1aaee1abd616325c8753c637955
Hannibal036 <109810162+Hannibal036@users.noreply.github.com>
2025-11-27 18:24:00 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'DeinName'
+description 'Persistent Vehicle System für ESX Legacy'
+version '1.0.0'
+
+server_scripts {
+    'server.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+dependencies {
+    'es_extended'
+}
